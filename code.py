90da61a41cf29340f8d3277c04e9850d87c99b49
Thea Flowers <thea@thea.codes>
2020-02-03 16:12:07 -0800

    Move the neopixel update to a periodic task
    This also changes up the pixel animation to a nice marching wheel animation.
    In my experiments, the PyPortal isn't really able to update the neopixels
    any faster than around 10hz due to the time it takes to draw items on
    the screen. This does, however, seem to make touch inputs much more
    responsive.

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 import time
 import board
+import math
 from adafruit_pyportal import PyPortal
 from adafruit_button import Button
 # from displayio import Group
@@ -7,6 +8,61 @@ import terminalio
 import neopixel
 import displayio
 from adafruit_bitmap_font import bitmap_font
+
+
+class PeriodicTask:
+    """Used to run a task every so often without needing to call
+    time.sleep() and waste cycles.
+
+    Sub-classes should implement `run`. You should call `update`
+    every loop.
+    """
+    def __init__(self, frequency):
+        self.frequency = frequency
+        self._last_update_time = 0
+
+    def update(self):
+        now = time.monotonic_ns()
+        next_update_time = self._last_update_time + int(1.0 / self.frequency * 1000000000)
+        if now > next_update_time:
+            self.run()
+            self._last_update_time = time.monotonic_ns()
+
+    def run(self):
+        pass
+
+
+class PixelPattern(PeriodicTask):
+    def __init__(self, frequency, strip):
+        super().__init__(frequency)
+        self.strip = strip
+        self._offset = 0
+        self.color = 0
+
+    def run(self):
+        # Fetch these first to avoid repeated lookups on properties.
+        color = self.color
+        strip = self.strip
+
+        # Normalize the color. It can come in as an integer (like 0xFFAABB) but
+        # we want a tuple.
+        if isinstance(color, int):
+            color = color.to_bytes(3, "big")
+
+        multiplier_up = (math.sin((self._offset % 21 / 20) * 2 * 3.14) + 1.0) * 0.5
+        multiplier_down = 1.0 - multiplier_up
+
+        for n in range(strip.n):
+            if n % 2 == 0:
+                multiplier = multiplier_up
+            else:
+                multiplier = multiplier_down
+
+            strip[n] = (int(color[0] * multiplier), int(color[1] * multiplier), int(color[2] * multiplier))
+
+        strip.show()
+        self._offset += 1
+
 
 arial_font = bitmap_font.load_font("/fonts/Arial-ItalicMT-17.bdf")
 
@@ -39,37 +95,26 @@ def create_buttons(size=60, offset=10):
     return buttons
 
 
-def chase_pattern(color, offset):
-    """Single color chase pattern.
-
-    Based on code from:
-        https://learn.adafruit.com/gemma-hoop-earrings/circuitpython-code
-    """
-    for i in range(num_pixels):
-        if ((offset + i) & (num_pixels)) < 2:
-            strip[i] = current_color
-        else:
-            strip[i] = 0
-    strip.show()
-    time.sleep(0.08)
-    offset += 1
-    if offset >= num_pixels:
-        offset = 0
-    return offset
-
-# PyPortal Initialization
-
-background_color = 0x0  # black
-brightness = 0.1        # 60%
-num_pixels = 5          # 5 pixel strip
-auto_write = False      # call strip.show() to change neopixel vals
-
+# NeoPixel strip initialization
 strip = neopixel.NeoPixel(
-    board.D4, num_pixels,
-    brightness=brightness,
-    auto_write=auto_write)
+    board.D4,
+    # The number of pixels on the strip.
+    n=24,
+    # Turn the brightness down to avoid hurting eyes
+    # and save on battery.
+    brightness=0.1,
+    # Requires calling strip.show() to change neopixel values.
+    auto_write=False)
 strip.fill(0)
 
+pixel_pattern = PixelPattern(
+    strip=strip,
+    # Update 10 times per second.
+    frequency=10,
+)
+
+# PyPortal Initialization
+background_color = 0x0  # black
 pyportal = PyPortal(default_bg=background_color)
 
 buttons = create_buttons()
@@ -123,8 +168,7 @@ while True:
                     pyportal.set_background(status_backgrounds[button.name])
 
                 print("Touched", button.name)
-                current_color = button.fill_color
+                pixel_pattern.color = button.fill_color
                 break
 
-    current_offset = chase_pattern(color=current_color, offset=current_offset)
-    time.sleep(0.05)
+    pixel_pattern.update()
